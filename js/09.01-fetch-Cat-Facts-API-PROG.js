37651209de0ab693ce0fde157187b27001ecbf1e
Colin Jaffe <colin.jaffe@gmail.com>
2024-07-02 19:21:24 -0400

    Resets for next cohort.

--- js/09.01-fetch-Cat-Facts-API-PROG.js
+++ js/09.01-fetch-Cat-Facts-API-PROG.js
@@ -1,5 +1,5 @@
 // Lesson 09.01 Cat Facts API
-// API (Application Programming Interface) involves an app requesting data from a server
+// API (Application Programming Interface)  involves an app requesting data from a server
 // data is returned to the add in JSON (JavaScript Object Notation)
 // JSON is basically a JS object where all the keys are in double quotes
 // The stringified object can travel over a network better, cuz it's just a string
@@ -13,49 +13,41 @@
 // hold random cat fact loaded from API
 
 // 0. Declare a var set equal to cat fact json:
-let catFactJsonStr = `{"fact":"The longest living cat on record according to the Guinness Book belongs to the late Creme Puff of Austin, Texas who lived to the ripe old age of 38 years and 3 days!","length":165}`;
-console.log("typeof(catFactJsonStr):", typeof catFactJsonStr); // string
-console.log("catFactJsonStr:", catFactJsonStr);
 
 // JSON vs. "regular JS object"
 // JSON.stringify(obj);
-// 1. Convert catFact json string to "regular" js object:
-const catFactObj = JSON.parse(catFactJsonStr);
-console.log("typeof(catFactObj):", typeof catFactObj); // object
-console.log("catFactObj:", catFactObj);
-let catFact = catFactObj.fact;
-console.log("catFact:", catFact); // The longest living cat on record according to the Guinness Book belongs to the late Creme Puff of Austin, Texas who lived to the ripe old age of 38 years and 3 days!
+// 1. Convert catFact json string to "regular" js object
 
 // 2. convert the object to JSON for transmission over a network
-//    JSON.stringify(obj) takes an obj w properties as its arg
+//    tJSON.stringify(obj) takes an obj w properties as its arg
 //    and returns the JSON version, where the keys are in double quotes
 //    so save the returned JSOn to a variable
-catFactJsonStr = JSON.stringify(catFactObj);
-console.log("typeof(catFactJsonStr):", typeof catFactJsonStr); // string
-console.log("catFactJsonStr:", catFactJsonStr);
 
-// grab the DOM elems: the 2 btns and the output div:
-const petInfoDiv = document.getElementById("pet-info");
-const catBtn = document.getElementById("cat-btn");
-catBtn.addEventListener("click", fetchCatFact);
-const dogBtn = document.getElementById("dog-btn");
-dogBtn.addEventListener("click", fetchDogPicture);
+console.log("pet:");
+// with the regular object, you have access to the properties
+console.log("pet.name:");
 
-function fetchCatFact() {
-  // fetch(url, method) returns a Promise (not the actual data yet)
-  // "GET" specifies how the data is sent from the
-  // server -- GET means the data rides along on the URL
-  fetch("https://catfact.ninja/fact", { method: "GET" })
-    // .then(callback) takes a callback (func) as its argument
-    // the argument of the callback is the returned JSON data)
-    // the fetch() method has its own methods: then() and json()
-    // .then(JSON) receives the JSON as the arg of its callback
-    // .then() runs once the Promise is fulfilled (data has arrived)
-    // .then() parses json str and returns a usable JS object
-    .then((catJson) => catJson.json())
-    // second .then() receives parsed object as arg of its callback
-    .then((catObj) => (petInfoDiv.textContent = catObj.fact));
-} // end fetchCatFact()
+// with the JSON, you do NOT yet have access to the properties
+// because they are stringified
+console.log("petJSON:"); // {"type":"bunny","name":"Thumper","age":3,"herbivore":true,"faveFoods":["carrots","blueberries","parsley"]}
+console.log("petJSON.name:"); // undefined
+
+// JSON.parse(JSONstring) works in the opposite direction of JSON.strinify(obj)
+// you pass the parse() method a JSON string and get back a "regular" JS obj
+// with properties that you can now access.  So, do the pet in reverse now:
+
+console.log("petObj:"); // { type:"bunny", name:"Thumper", age:3, herbivore: true, faveFoods":["carrots","blueberries","parsley"] }
+
+console.log("petObj.name:"); // Thumper
+console.log("petObj.faveFoods[1]:"); // blueberries
+
+// grab the DOM elems: the 2 btns and the output div.
+
+// Add an event listener to run a displayCatFact function on click for the cat
+// button.
+
+// For our cat fact function, run a fetch function (built into browsers) to hit up the Cat Facts API. Then use the first .then callback to convert from JSON to a JS object and the second .then callback to put the cat fact on the DOM.
+
 
 // LAB CHALLENGE: Make the "Get Dog Pic" button work
 // clicking btn should display a diff random dog pic each time
@@ -63,15 +55,3 @@ function fetchCatFact() {
 // Dog Pic API Random Result:
 // sample of random result from Dog API :
 // {"message":"https:\/\/images.dog.ceo\/breeds\/keeshond\/n02112350_4005.jpg","status":"success"}
-
-function fetchDogPicture() {
-  let dogUrl = "https://dog.ceo/api/breeds/image/random";
-  petInfoDiv.innerHTML = "";
-  fetch(dogUrl, { method: "GET" })
-    .then((jsn) => jsn.json())
-    .then((obj) => {
-      let pic = new Image();
-      pic.src = obj.message;
-      petInfoDiv.appendChild(pic);
-    });
-}
